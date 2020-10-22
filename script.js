3574b297b1cdf58de8f361002766dfdab1045996
Roman <www.tolkoya@gmail.com>
2020-10-22 11:52:45 +0300

    fix(script.js): recreate methods of classes

--- script.js
+++ script.js
@@ -1,110 +1,118 @@
 class ToDoForm {
-  constructor(selector, onSubmit) {
+  constructor (selector, onSubmit = () => {}) {
     this.selector = selector;
+    this.onSubmit = onSubmit;
+    this.rootElement = selector();
   }
 
   createForm () {
     let form = document.createElement('form');
     form.classList.add('form');
-    this.selector.append(form);
+    this.rootElement.append(form);
     this.form = form;
-  }
 
-  createNameInput () {
     let nameInput = document.createElement('input');
     nameInput.type = 'text';
     nameInput.name = 'name';
     nameInput.placeholder = 'My new todo';
     nameInput.classList.add('input');
     this.form.append(nameInput);
+    nameInput.addEventListener('input', (e) => {
+      this.valueName = e.target.value;
+      console.log(this.valueName);
+    });
     this.nameInput = nameInput;
-  }
 
-  createTagsInput () {
     let tagsInput = document.createElement('input');
     tagsInput.type = 'text';
     tagsInput.name = 'tags';
     tagsInput.placeholder = 'todo, important, other...';
     tagsInput.classList.add('input');
     this.form.append(tagsInput);
+    tagsInput.addEventListener('input', (e) => {
+      this.valueTags = e.target.value;
+      console.log(this.valueTags);
+    });
     this.tagsInput = tagsInput;
-  }
 
-  createSubmitButton () {
     let submitButton = document.createElement('button');
     submitButton.type = 'button';
     submitButton.textContent = 'Create';
     submitButton.classList.add('button');
+    submitButton.addEventListener('click', () => {
+      this.onSubmit({name: this.valueName, tags: this.valueTags});
+      this.cleanInputs();
+    });
     this.form.append(submitButton);
     this.submit = submitButton;
-  }
-
-  getNameInputValue () {
-    this.nameInputValue = this.nameInput.value;
-    console.log(this.nameInputValue);
-  }
-
-  getTagsInputValue () {
-    this.tagsInputValue = this.tagsInput.value;
-    console.log(this.tagsInputValue);
   }
 
   cleanInputs () {
     this.nameInput.value = '';
     this.tagsInput.value = '';
   }
-
-  getValues () {
-    this.getNameInputValue();
-    this.getTagsInputValue();
-    this.cleanInputs();
-  }
 }
 
-let toDoForm = new ToDoForm(document.querySelector('body'));
+function onSubmit (obj) {
+  console.log(obj);
+  return obj;
+}
+
+let toDoForm = new ToDoForm(() => document.querySelector('body'), onSubmit);
 
 // --------------------------------------
 // Это будет работать в последнем классе!
 toDoForm.createForm();
-toDoForm.createNameInput();
-toDoForm.createTagsInput();
-toDoForm.createSubmitButton();
-
-toDoForm.submit.addEventListener('click', () => {
-  toDoForm.getValues();
-});
 // --------------------------------------
 
 class ToDoList {
-  constructor(selector) {
+  constructor(selector, initialState = []) {
     this.selector = selector;
+    this.toDoItems = [...initialState];
   }
 
-  createToDoList () {
-    let toDoList = document.createElement('div');
-    toDoList.classList.add('toDoList');
-    this.selector.append(toDoList);
+  // get toDoItems() {
+  //   return this.toDoItems;
+  // }
+  //
+  // set toDoItems(value) {
+  //   this.toDoItems = value;
+  //   this.checkIsEmpty();
+  //   console.log('проверка');
+  // }
+
+  checkIsEmpty() {
+    // 1. проверить длинну массива
+    // 2. если дринна 0 - очистить контейнер, отрендерить строку с текстом
+    // 3. иначе ничего
   }
 
-  createToDoName () {
+  renderEmptyLabel() {
 
   }
 
-  createToDoTags () {
+  renderToDoItems() {
 
   }
 
-  createDoneCheckboxes () {
+  renderToDoItem() {
 
   }
 
-  createDeleteButtons () {
+  addToDoItem() {
 
   }
+
+  createList () {
+    let list = document.createElement('div');
+    list.classList.add('list');
+    this.selector.append(list);
+  }
+
 }
 
 let toDoList = new ToDoList(document.querySelector('body'));
-toDoList.createToDoList();
+toDoList.createList();
 
 class ToDosHandle {
   constructor() {
