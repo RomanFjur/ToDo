dd9d65696eb63a3e6efc2841d358012338c766dd
Roman <www.tolkoya@gmail.com>
2020-10-28 14:09:12 +0300

    feat: add comments and task

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ class IdGenerator {
     let newId = '';
     const avaliableSymbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     for (var i = 0; i < length; i++) {
-      let randomSymbol = Math.floor(Math.random()*36);
-      newId = `${newId}${avaliableSymbols[randomSymbol]}`;
+      let randomSymbolIndex = Math.floor(Math.random() * avaliableSymbols.length);
+      newId = `${newId}${avaliableSymbols[randomSymbolIndex]}`;
     }
     return newId;
   }
@@ -88,6 +88,7 @@ class ToDoForm {
         this.cleanInputs();
       }
     });
+    // В отдельную функцию ^
 
     this.form.append(submitButton);
     this.submit = submitButton;
@@ -146,7 +147,7 @@ class ToDoList {
     }
   }
 
-  // nethod to render string with message about empty todolist
+  // method to render string with message about empty todolist
   renderEmptyLabel() {
     let messageOfEmpty = document.createElement('p');
     messageOfEmpty.textContent = "You don't need to do anything!";
@@ -163,7 +164,7 @@ class ToDoList {
     // Пока не продумывал
   }
 
-  renderToDoItem(toDoItem) {
+  renderToDoItem(toDoItem, onMarkAsDone) {
 
     const toDo = document.createElement('div'),
           checkboxLabel = document.createElement('label'),
@@ -186,7 +187,7 @@ class ToDoList {
     // 2. С помощью reduce собрать строку с тегами из массива с тегами +-
 
     toDoName.textContent = `${toDoItem.name}`;
-    toDoTags.textContent = `${toDoItem.tags}`;
+    toDoTags.textContent = `${toDoItem.tags}`; // работать с массивом тут
 
     this.list.append(toDo);
     toDo.append(checkboxLabel);
@@ -197,10 +198,10 @@ class ToDoList {
     toDo.append(toDoTags);
 
     toDo.dataset.id = toDoItem.id;
-    this.toDos.push(toDo);
+    this.toDos.push(toDo); // при удалении обратиться к parentNode, считать id
 
     toDoCheckbox.addEventListener('click', () => {
-      if (toDoItem.toggle == false) {
+      if (toDoItem.toggle === false) {
         toDoItem.toggle = true;
         toDoName.classList.toggle('nameChecked');
         toDoTags.classList.toggle('tagsChecked');
@@ -209,12 +210,10 @@ class ToDoList {
         toDoName.classList.toggle('nameChecked');
         toDoTags.classList.toggle('tagsChecked');
       }
-    });
+    }); // пока что так, потом переделать (вынести в отдельную функцию) onMarkAsDone!!
 
     toDoDeleteButton.addEventListener('click', () => {
       this.removeElementById(toDoItem.id);
-      toDoCheckbox.removeEventListener('click', () => {});
-      toDoDeleteButton.removeEventListener('click', () => {});
     });
 
     // 1. Добавить аргумент toDoItem который использовать в качестве элемента +
@@ -232,11 +231,18 @@ class ToDoList {
         this.toDoItems = this.toDoItems;
       }
     });
+
+    // 1. Вместо массива toDos - записываем в поле element сам DOM-элемент
+    // 2. Переписываем функцию под filter
+    // для удаления элемента (через remove если поле element есть) или если этого поля нет (начинаем искать с помощью селектора);
+    // Новый класс для (localStorage) ToDoSaver (getToDoItems и setToDoItems(array));
+
+    // ВСЕГДА работаем от данных! Исходя из состояния!
   }
 
   addElement(data) {
     const array = [...this.toDoItems, data];
-    this.toDoItems = array;
+    this.toDoItems = array; //в Конец (не менять ссылку)!
 
     const toDoItemIndex = this.toDoItems.indexOf(data);
     this.toDoItems[toDoItemIndex].toggle = false;
@@ -249,7 +255,8 @@ class ToDoList {
     this.toDoItems[toDoItemIndex].todoNumber = toDoItemIndex + 1;
 
     const toDoItem = this.toDoItems[toDoItemIndex];
-    localStorage.setItem(`todo №${toDoItem.todoNumber}`, toDoItem);
+    localStorage.setItem(`todo №${toDoItem.todoNumber}`, toDoItem); // к строке
+
     this.renderToDoItem(toDoItem);
   }
 }
@@ -261,8 +268,16 @@ toDoForm.setOnSubmit((data) => {
   toDoList.addElement(data);
 });
 
-// 1. Добавить удаление элементов +
-// 2. Добавить функцию GenerateNewId (реализацию) +
-// 3. Добавить id (в массив) +
-// 4. добавить функцию removeElementById (чистые функции!!! приходит только id!!!) +-?
-// 5. состояние хранить в todoItems (функция toggleIsDoneById) +
+// 1. Теория: Что такое генераторы (генератор случайных чисел);
+// 2. Добиваю практику:
+//    - class ToDoSaver для работы с localStorage;
+//    - перенос вызова toDoItems = array в конец, так же замена работы функции на работу с array;
+//    - добавить поле element со ссылкой на DOM-элемент;
+//    - запись id как data-attribute (закончить с удалением по id);
+//    - реализовать onMarkAsDone в renderToDoItem;
+//
+//                    СПЕРВА node.js!!!
+//
+// 3. https://tproger.ru/translations/javascript-important-concepts/
+// 4. https://proglib.io/p/beginners-guide-to-node-js/
+// 5. https://nodejs.dev/learn/introduction-to-nodejs
